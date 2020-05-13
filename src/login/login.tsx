b7bc685ce3a4100f3efb3efcd3b851f11de323f5
mannix-lei <zx2351634862@outlook.com>
2020-05-13 16:10:23 +0800

    add login

--- src/login/login.tsx
+++ src/login/login.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import style from './login.module.scss';
+import { Button } from 'antd';
 
 export const Login = () => {
     return (
-        <div>
-            login
+        <div className={style.body}>
+            <div className={style.btnGroup}>
+                <Button>sign in</Button>
+                <Button>sign up</Button>
+            </div>
+            {/* <div className={style.login}>
+                
+            </div> */}
         </div>
     )
 }
