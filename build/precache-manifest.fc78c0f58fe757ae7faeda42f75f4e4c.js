24fafbca9fabe2e76245f37af593103127632341
mannix-lei <zx2351634862@outlook.com>
2020-05-14 13:22:09 +0800

    fix dockerfile

---
+++ build/precache-manifest.fc78c0f58fe757ae7faeda42f75f4e4c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "335ad46fc9cfb79b49b9a638469978b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93c7c799f56ba628546c",
+    "url": "/static/css/2.d7262850.chunk.css"
+  },
+  {
+    "revision": "3887c9a2f1af1835d1af",
+    "url": "/static/css/main.3f60da21.chunk.css"
+  },
+  {
+    "revision": "93c7c799f56ba628546c",
+    "url": "/static/js/2.68db3e80.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.68db3e80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3887c9a2f1af1835d1af",
+    "url": "/static/js/main.4bee650e.chunk.js"
+  },
+  {
+    "revision": "af34574360f2e8c45495",
+    "url": "/static/js/runtime-main.916fb653.js"
+  },
+  {
+    "revision": "312ef451491b843549dd9928980e0c79",
+    "url": "/static/media/mountain.312ef451.jpg"
+  }
+]);
